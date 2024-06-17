09bc756f744416e79dc3c8b247c4c11b5d9cce3c
amratansh12 <ashri1205@gmail.com>
2024-06-17 17:52:29 +0530

    Fixing minor issues

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -5,7 +5,7 @@ type Image = {
   alt_description: string;
   created_at: string;
   urls: {
-    full: string;
+    small: string;
   };
   id: string;
 };
@@ -14,14 +14,14 @@ const Gallery = () => {
   const [images, setImages] = useState<Image[]>([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const observer = useRef();
+  const lastImageRef = useRef(null);
 
   const loadImages = async () => {
     setLoading(true);
 
     try {
       const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
-      const url = `https://api.unsplash.com/photos?page=${page}&per_page=4&client_id=${accessKey}`;
+      const url = `https://api.unsplash.com/photos?page=${page}&per_page=10&client_id=${accessKey}`;
       const response = await axios.get(url);
 
       setImages((prevImages) => [...prevImages, ...response.data]);
@@ -42,51 +42,64 @@ const Gallery = () => {
     return `${newDate.getDay()}-${newDate.getMonth()}-${newDate.getFullYear()}`;
   };
 
-  const lastImageRef = useCallback(
-    (node) => {
-      if (loading) return;
+  const callbackFunction = (entries: any) => {
+    if (entries[0].isIntersecting) {
+      setInterval(() => {
+        setPage((prevPage) => prevPage + 1);
+      }, 2000);
+      //   console.log(entries);
+    }
+  };
 
-      if (observer.current) observer.current.disconnect();
+  const options = {
+    threshold: 1,
+  };
 
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          setPage(page + 1);
-        }
-      });
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunction, options);
 
-      if (node) observer.current.observe(node);
-    },
-    [loading]
-  );
+    if (lastImageRef.current) {
+      observer.observe(lastImageRef.current);
+    }
+
+    return () => {
+      if (lastImageRef.current) {
+        observer.unobserve(lastImageRef.current);
+      }
+    };
+  }, [lastImageRef, options]);
 
   return (
-    <div className="w-full flex-1 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {images.map((image, index) => (
-        <div
-          className="bg-slate-300 flex flex-col items-center justify-center p-4 rounded-md shadow-sm space-y-2"
-          key={image.id}
-          ref={index === images.length - 1 ? lastImageRef : null}
-        >
-          <img
-            src={image.urls.full}
-            alt="Image"
-            className="w-[300px] aspect-square"
-          />
-          <p className="font-bold text-center underline text-sm">
-            {image.alt_description}
-          </p>
-          <p className="font-bold text-sm">
-            Created at {parseDate(image.created_at)}
-          </p>
-        </div>
-      ))}
-
+    <>
+      <div className="w-full flex-1 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {images.map((image, index) => {
+          return (
+            <div
+              className="bg-slate-300 flex flex-col items-center justify-center p-4 rounded-md shadow-sm space-y-2"
+              key={image.id}
+              ref={index === images.length - 1 ? lastImageRef : null}
+            >
+              <img
+                src={image.urls.small}
+                alt="Image"
+                className="w-[300px] aspect-square"
+              />
+              <p className="font-bold text-center underline text-sm">
+                {image.alt_description}
+              </p>
+              <p className="font-bold text-sm">
+                Created at {parseDate(image.created_at)}
+              </p>
+            </div>
+          );
+        })}
+      </div>
       {loading && (
-        <p className="w-full flex items-center justify-center p-1 text-indigo-400">
-          Loading
+        <p className="w-full flex items-center justify-center p-1 text-black">
+          Loading...
         </p>
       )}
-    </div>
+    </>
   );
 };
 
